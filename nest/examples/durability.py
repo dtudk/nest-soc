Fix pressure gradients Fixing the calculation of pressure gradients and effect on Ni agglomeration

nest/examples/durability.py
@@ -43,7 +43,7 @@ def durability_test():
             r0_ed=300e-9,
             r0_el=350e-9,
             epsilon=0.3,
-            alpha=3.98e3,
+            alpha=3.04e8,
             r_ed_max=500e-9,
         ),
     )
@@ -95,7 +95,7 @@ def durability_test():
 
     # Define cell
     cellModel = cell.Cell(
-        16e-4, Ni_YSZ, (YSZ, YSZ_CGO, CGO, Crofer22, CrScale), LSCF_CGO, elements=5
+        16e-4, Ni_YSZ, (YSZ, YSZ_CGO, CGO, Crofer22, CrScale), LSCF_CGO, elements=10
     )
 
     # Define boundary conditions
@@ -116,9 +116,61 @@ def durability_test():
     start_time = process_time()
     solution = cellModel.solve_for_time(conditions, "current", 5000)
     print(f"Computation time : {process_time() - start_time} seconds")
+    
+    """
+    
+    fig, (ax1,ax2,ax3) = plt.subplots(1,3, figsize=(6.4*3,4.8))
+    
+    r_pol_fuel = np.zeros((len(solution.t),cellModel.elements))
+    r_pol_air = np.zeros((len(solution.t),cellModel.elements))
+    r_ohm = np.zeros((len(solution.t),cellModel.elements))
 
-    fig, ax = plt.subplots()
+    l = np.array([1/cellModel.elements*(i+1) - 0.5*1/cellModel.elements for i in range(cellModel.elements)])
+    r_pol_fuel_0 = Ni_YSZ.degradation.pol_deg(Ni_YSZ.degradation.m0)
+    # Breakaway corrosion
+    rho_Cr2O3 = 5220
 
+    M_o = 15.9994   # g/mol
+    M_Cr = 51.9961  # g/mol
+    
+    C_breakaway = 369.01 * (conditions.T**(-1.205)) * 100
+    h_IC = 3e-4#2.5e-3
+    L_cell = 4e-2
+    w_cell = 4e-2
+    n_segment = cellModel.elements
+    h = h_IC / (1 + h_IC/(L_cell/n_segment) + h_IC/w_cell)       # Eq.(6.5) Reddy thesis and https://doi.org/10.1002/(SICI)1521-4176(200004)51:4%3C224::AID-MACO224%3E3.0.CO;2-B
+    C_0 = 22.92
+    rho_interconnect = 7700 # kg/m3
+    h_max = (C_0 - C_breakaway)/100 * rho_interconnect * h/2 * (3/2) * (M_o / M_Cr) / rho_Cr2O3
+
+    for t in range(len(solution.t)):
+        for i in range(cellModel.elements):
+            r_pol_fuel[t][i] = Ni_YSZ.degradation.pol_deg(solution.y[i*2][t])/r_pol_fuel_0
+            r_pol_air[t][i] = solution.y[1+i*2][t]
+            r_ohm[t][i] = 1-solution.y[2*cellModel.elements+i][t]/h_max
+    
+    for t in range(len(solution.t)):
+        if solution.t[t] > 0 and solution.t[t] < 1:
+            pass
+        else:
+            ax1.plot(l,r_pol_fuel[t],label=f"{round(solution.t[t])} h")
+            ax1.set_xlabel("Normalized cell length [-]")
+            ax1.set_ylabel("Fuel electrode area ratio [-]")
+
+            ax2.plot(l,r_pol_air[t],label=f"{round(solution.t[t])} h")
+            ax2.set_xlabel("Normalized cell length [-]")
+            ax2.set_ylabel("Air electrode area ratio [-]")
+
+            ax3.plot(l,r_ohm[t],label=f"{round(solution.t[t])} h")
+            ax3.set_xlabel("Normalized cell length [-]")
+            ax3.set_ylabel("Normalized corrosion thickness [-]")
+
+
+    plt.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0)
+    """
+    
+    """
+    fig, ax = plt.subplots(figsize=(6.4*3,4.8))
     # Solve 1D problem for material conditions
     conditions.V = 0.75
     y_matrix = np.transpose(solution.y)
@@ -140,7 +192,45 @@ def durability_test():
     )
     plt.title("94%H2+6%H2O | 21%O2+79%N2 | 700 degC", fontsize=14, loc="center")
     plt.legend()
+    """
 
+    # Solve 1D problem for material conditions
+    
+    fig, ax = plt.subplots(figsize=(6.4,4.8))
+    conditions.V = 0.75
+    y_matrix = np.transpose(solution.y)
+    eta_fuel = np.zeros(len(y_matrix))
+    eta_air = np.zeros(len(y_matrix))
+    eta_ohm = np.zeros(len(y_matrix))
+    ocv = np.zeros(len(y_matrix))
+    for i, y in enumerate(y_matrix):
+        steady_solution = cellModel.solve_time_step(y, conditions, "current")
+        eta_fuel[i] = np.sum(steady_solution[8]) / cellModel.elements
+        eta_air[i] = np.sum(steady_solution[9]) / cellModel.elements
+        eta_ohm[i] = np.sum(steady_solution[10:]) / cellModel.elements
+        ocv[i] = np.sum(steady_solution[0]) / cellModel.elements + eta_fuel[i] + eta_air[i] + eta_ohm[i]
+        
+        conditions.V = np.sum(steady_solution[0]) / cellModel.elements
+
+    ax.plot(solution.t, ocv, label="OCV")
+    ax.plot(solution.t, ocv - eta_ohm, label="Ohmic")
+    ax.fill_between(solution.t,ocv,ocv-eta_ohm, alpha=.5, color="C1")
+    ax.plot(solution.t, ocv - eta_ohm - eta_fuel, label="Fuel")
+    ax.fill_between(solution.t,ocv-eta_ohm, ocv - eta_ohm - eta_fuel , alpha=.5, color="C2")
+    ax.plot(solution.t, ocv - eta_ohm - eta_fuel- eta_air, label="Air")
+    ax.fill_between(solution.t, ocv - eta_ohm - eta_fuel, ocv - eta_ohm - eta_fuel- eta_air , alpha=.5, color="C3")
+    ax.set_xlabel("Time (h)")
+    ax.set_ylabel("Voltage (V)")
+
+    script_dir = Path(__file__).parent
+    data = pd.read_csv(script_dir / "data/700C_75FU_40AIR.csv")
+    ax.scatter(
+        data["t"], data["V"], label="Experiment", edgecolors="b", facecolor="none"
+    )
+
+    plt.title("94%H2+6%H2O | 21%O2+79%N2 | 700 degC", fontsize=14, loc="center")
+    plt.legend()
+    
     return plt.show()
 
 